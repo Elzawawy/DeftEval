7338df8907fbb7ee3927f2bbfdb151c9312beeb4
Elzawawy <amr22.elzawawy@gmail.com>
2020-01-10 22:33:12 +0200

    Bug Fixes in Spacy Classififer, Add Reporting Spacy Notebook.

--- source/classifiers.py
+++ source/classifiers.py
@@ -16,10 +16,10 @@ class DeftSpacyClassifier(object):
             self.nlp = en_core_web_lg.load()     # load default langauge model.
             print("Loaded default model '%s'" % en_core_web_lg)
         
-        if "textcat" not in nlp.pipe_names:
+        if "textcat" not in self.nlp.pipe_names:
             self.textcat = self.nlp.create_pipe("textcat", 
                 config={"exclusive_classes": True, "architecture": "simple_cnn"})
-            self.nlp.add_pipe(textcat, last=True)
+            self.nlp.add_pipe(self.textcat, last=True)
         else:
             self.textcat = self.nlp.get_pipe("textcat")
 
@@ -30,16 +30,15 @@ class DeftSpacyClassifier(object):
         self.textcat.add_label(self.POSITIVE)
         self.textcat.add_label(self.NEGATIVE)
 
-    def fit(self, train_texts, dev_texts, train_cats, dev_cats, 
-        n_iter=20, loss_tol=0.005, output_dir = None):
+    def fit(self, train_texts, dev_texts, train_cats, dev_cats, n_iter=20, loss_tol=0.005, output_dir = None):
 
         train_data = list(zip(train_texts, [{"cats": cats} for cats in train_cats]))
         # get names of other pipes to disable them during training
-        other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "textcat"]
+        other_pipes = [pipe for pipe in self.nlp.pipe_names if pipe != "textcat"]
         with self.nlp.disable_pipes(*other_pipes):  # only train textcat
             optimizer = self.nlp.begin_training()
         print("Training the model...")
-        print("{:^5}\t{:^5}\t{:^5}\t{:^5}\t{:^5}".format("LOSS", "P", "R", "F"))
+        print("{:^5}\t{:^5}\t{:^5}\t{:^5}".format("LOSS", "P", "R", "F"))
         # Yield an infinite series of compounding values.
         batch_sizes = compounding(32.0, 100.0, 1.001)
         # The Training Loop
@@ -50,10 +49,10 @@ class DeftSpacyClassifier(object):
             batches = minibatch(train_data, size=batch_sizes)
             for batch in batches:
                 texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer, drop=0.2, losses=losses)
+                self.nlp.update(texts, annotations, sgd=optimizer, drop=0.2, losses=losses)
             with self.textcat.model.use_params(optimizer.averages):
                 # evaluate on the dev data split off in load_data()
-                scores = evaluate(nlp.tokenizer, textcat, dev_texts, dev_cats)
+                scores = self.evaluate(self.nlp.tokenizer, dev_texts, dev_cats)
             print("{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}".format(
                     losses["textcat"],
                     scores["textcat_p"],
@@ -100,7 +99,7 @@ class DeftSpacyClassifier(object):
             f_score = 2 * (precision * recall) / (precision + recall)
         return {"textcat_p": precision, "textcat_r": recall, "textcat_f": f_score}
 
-    def score(self, dev_texts, dev_cats):
+    def score(self, dev_texts, dev_labels):
         if(self.classifier_output is None):
             raise NotImplementedError("You must save the model to an output directory after training, in order to use this function. Other ways are not implemented currently.")
         nlp = spacy.load(self.classifier_output)
@@ -110,4 +109,4 @@ class DeftSpacyClassifier(object):
             predict_doc = 1 if doc.cats[self.POSITIVE] > doc.cats[self.NEGATIVE] else 0
             predicted.append(predict_doc)
         # Print the resulting scores.
-        print(classification_report(list(dev_cats), predicted))
+        print(classification_report(list(dev_labels), predicted))
